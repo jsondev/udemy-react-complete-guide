421fb425fe7cca295b81d6ff972112749c44270c
James Anderson <jsondev1.0@outlook.com>
2018-11-24 18:11:10 -0500

    removed hardcode and added state and click handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+state = {
+  persons: [
+    {name: "Max", age:"24"},
+    {name: "Manu", age:"29"},
+    {name: "Stephanie", age:"30"}
+  ]
+}
+
+switchNameHandler = () =>{
+console.log('Was clicked!');
+}
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working!!</p>
-        <Person name = "Max" age="24"><p>Testing props.children</p></Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name ={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name ={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name ={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
