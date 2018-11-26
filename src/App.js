0aa67e61442d40f21adcf6001f43f0048e55ce3f
James Anderson <jsondev1.0@outlook.com>
2018-11-26 07:52:11 -0500

    added comments for the nameChangedHandler

--- src/App.js
+++ src/App.js
@@ -11,25 +11,43 @@ class App extends Component {
         ]
     }
 
+
+    /**
+     *  Updates a single person
+     */
     nameChangedHandler = (event, id) => {
+        
+        //  finds the single person to update by their id
         const personIndex = this.state.persons.findIndex(p => {
             return p.id === id;
         });
 
+        //  creates a new object with the fetched person
         const person = {
             ...this.state.persons[personIndex]
         }
 
+        // alternative approach to creating the fetched person object
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        //  updates the name of the fetched person
         person.name = event.target.value;
 
+        //  creates a copy of the persons array
         const persons = [...this.state.persons];
+
+        // point to where the update should take place
         persons[personIndex] = person;
 
+        // update the fetched person in the persons array
         this.setState({
             persons: persons
         });
     }
 
+    /**
+     * Deletes a single person
+     */
     deletePersonHandler = (personIndex) => {
         // const persons = this.state.persons.slice();
         const persons = [...this.state.persons];
