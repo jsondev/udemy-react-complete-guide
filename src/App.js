a0b8ea86f7b7d8c4314210fe679afb5a416b81bc
James Anderson <jsondev1.0@outlook.com>
2018-12-02 10:26:40 -0500

    removed radium for CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -76,11 +75,7 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
+            cursor: 'pointer'
         }
         let persons = null;
 
@@ -95,10 +90,7 @@ class App extends Component {
                 </div>
             )
             style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            }
+        
         }
 
         const classes = [];
@@ -110,16 +102,16 @@ class App extends Component {
         }
 
         return (
-            <StyleRoot>
+            
                 <div className="App">
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join(' ')} >This is working!!</p>
                     <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
                     {persons}
                 </div>
-            </StyleRoot>
+            
         );
     }
 }
 
-export default Radium(App);
+export default App;
