c8e7dd49fc56dcc743d8d94a24f6be1167db04cc
James Anderson <jsondev1.0@outlook.com>
2018-12-02 10:45:41 -0500

    implemented CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -95,15 +95,15 @@ class App extends Component {
 
         const classes = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            classes.push(styles.red);
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            classes.push(styles.bold);
         }
 
         return (
             
-                <div className="App">
+                <div className={styles.App}>
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join(' ')} >This is working!!</p>
                     <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
